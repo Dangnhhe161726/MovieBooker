c1c841fcc3b1c4736c11aaebb0835174fa871c01
Groot2308 <danghoan2382002@gmail.com>
2024-06-17 10:32:52 +0700

    Front end Register

--- MovieBooker-backend/MovieBooker/Pages/Register.cshtml.cs
+++ MovieBooker-backend/MovieBooker/Pages/Register.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MovieBooker.Models;
+using StackExchange.Redis;
 
 namespace MovieBooker.Pages
 {
     public class RegisterModel : PageModel
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConnectionMultiplexer _redisConnection;
+        private readonly HttpClient _httpClient;
+
+        public RegisterModel(IHttpClientFactory httpClientFactory, IConnectionMultiplexer redisConnection, HttpClient httpClient)
+        {
+            _httpClientFactory = httpClientFactory;
+            _redisConnection = redisConnection;
+            _httpClient = httpClient;
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
         public void OnGet()
         {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var loginData = new { User.Email, User.Password };
+
+            var response = await httpClient.PostAsJsonAsync("https://localhost:5000/api/User/SignUp", loginData);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Login");
+            }
+            return Page();  
         }
     }
 }

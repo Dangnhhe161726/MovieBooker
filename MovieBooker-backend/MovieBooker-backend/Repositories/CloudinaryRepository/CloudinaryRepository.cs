bae53053a7c96954982ebb38bd3e291702509a26
Nguyễn Hồng Đăng <109275012+Dangnhhe161726@users.noreply.github.com>
2024-06-18 20:14:42 +0700

    Update service upload image to cloud

---
+++ MovieBooker-backend/MovieBooker-backend/Repositories/CloudinaryRepository/CloudinaryRepository.cs
@@ -0,0 +1,28 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MovieBooker_backend.Repositories.CloudinaryRepository
+{
+    public class CloudinaryRepository : ICloudinaryRepository
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public CloudinaryRepository(Cloudinary cloudinary)
+        {
+            _cloudinary = cloudinary;
+        }
+
+        public async Task<UploadResult> UploadImageAsync(Stream imageStream, string fileName)
+        {
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(fileName, imageStream),
+            };
+
+            var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            return uploadResult;
+        }
+    }
+}

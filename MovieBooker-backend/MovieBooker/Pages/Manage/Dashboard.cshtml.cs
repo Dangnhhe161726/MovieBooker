a30b33f7f749a55f30556f544bf6a64f7f29792b
CaoThuLuDau <npl.working@gmail.com>
2024-07-02 14:52:09 +0700

    Add UI for Dashboard

---
+++ MovieBooker-backend/MovieBooker/Pages/Manage/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MovieBooker.Pages.Manage
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
